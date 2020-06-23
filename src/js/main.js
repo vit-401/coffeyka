787c2da39eb854750ea587fb096f4f4623b82741
vit-401 <vetas-tektonik1@gmail.com>
2020-06-23 18:40:04 +0300

    added price section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 $(function(){
-
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 1,
+    slidesPerView: 4,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '">-</span>' +
+              '<span class="tes">-</span> ' +
+              '<span class="' + totalClass + '">-</span>';
+  }
+  });
+  
 });
